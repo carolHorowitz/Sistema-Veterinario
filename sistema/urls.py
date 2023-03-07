dccb1ed35cab43e948fea32f32a2b2917840b623
Carol Horowitz <carolvet.horowitz@gmail.com>
2023-03-06 22:43:16 -0300

    implementando metodo POST

--- sistema/urls.py
+++ sistema/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('cadastro/', views.cadastro, name='cadastro'),
     path('login/', views.login, name='login'),
+    path('validacao/', views.validacao, name='validacao')
 ]
     
--- sistema/views.py
+++ sistema/views.py
@@ -9,3 +9,10 @@ def cadastro(request):
 
 def login(request):
     return render(request, 'login.html')    
+
+def validacao(request):
+    nome = request.POST.get('nome')
+    email = request.POST.get('email')
+    senha = request.POST.get('senha')
+
+    return HttpResponse(f"{nome} {email} {senha}")
